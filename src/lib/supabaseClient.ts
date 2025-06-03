769071d4b7d0e2c2b901c087acfe4f41a1c0402f
alexgabe-dev <gaborsanyi08@gmail.com>
2025-06-03 16:13:10 +0200

    vercel v2

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://efgharfzrmpfzlxavauu.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmZ2hhcmZ6cm1wZnpseGF2YXV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5NDIyMDMsImV4cCI6MjA2NDUxODIwM30.V9ZeyrtyPcQ-bcfmLYmRygtK3Wb0IbzkowtTHkq48cQ';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
  
 export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
